de9e88b1400dbd56d262d2e2e931e155efb2d997
MahdiKarami <67632452+mahdikarami8484@users.noreply.github.com>
2025-09-18 19:53:53 +0330

    hook game function

---
+++ hooks/gameFuncHooks.cpp
@@ -0,0 +1,27 @@
+﻿#include "gameFuncHooks.h"
+#include <iostream>
+#include <intrin.h>
+
+decltype(&hkSetResolution) oSetResolution = nullptr;
+
+void WINAPI hkSetResolution(void* res) {
+	int* data = (int*)res;
+
+	int originalWidth = data[0];
+	int originalHeight = data[1];
+
+	data[0] = 1920;
+	data[1] = 1080;
+
+	oSetResolution(res);
+}
+
+void SetupGameHooks() {
+	uintptr_t gtaBase = (uintptr_t)GetModuleHandleA(NULL);
+	uintptr_t addrSetResolution = gtaBase + 0x3F6CB0;
+
+	oSetResolution = reinterpret_cast<decltype(&hkSetResolution)>(addrSetResolution);
+
+	extern HookManager gHooks;
+	gHooks.AddHook(reinterpret_cast<void**>(&oSetResolution), hkSetResolution);
+}
---
+++ hooks/gameFuncHooks.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "../hookManager.h"
+
+struct ResolutionInfo {
+    int width;
+    int height;
+    int depth;
+    int unknown;
+};
+
+void __stdcall hkSetResolution(void* res);
+//typedef void(WINAPI* SetResolutionFn)(void* res);
+//extern SetResolutionFn oSetResolution;
+extern decltype(&hkSetResolution) oSetResolution;
+
+void SetupGameHooks();
